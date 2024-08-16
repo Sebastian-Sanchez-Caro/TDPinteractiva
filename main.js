ccbf18d91f479bcdea8aee3c1266c0536494739b
Sebastian-Sanchez-Caro <164094988+Sebastian-Sanchez-Caro@users.noreply.github.com>
2024-08-15 20:16:38 -0500

    1er actuaclizacion

---
+++ main.js
@@ -0,0 +1,11 @@
+function mensaje1(){
+alert("Bienvenido a mi tarjeta de presentacion!")    
+}
+
+function mensaje2(){
+    alert("Bienvenido a TU tarjeta de presentacion")
+}
+
+function mensaje3(){
+    alert("Bienvenido a la tarjeta de presentacion de el")
+}
